a6879b595e897bc01d41cd0d76c6e3f5eb8aa9b1
tokiloki <codebadger1@gmail.com>
2021-12-13 11:17:37 +0200

    basic aritmetic functions added

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function addition(num1, num2){
 function subtract(num1, num2){
     return num1 - num2;
 }
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
