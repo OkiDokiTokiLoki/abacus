e38dfe15b81ecebb49bc65cb18dc2067ac432528
tokiloki <codebadger1@gmail.com>
2021-12-15 11:58:37 +0200

    event key handlers implement

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const buttons = document.querySelector('.buttons');
 const operationDisplay = document.querySelector('.operation');
 const resultDisplay = document.querySelector('.result');
 
@@ -13,4 +14,39 @@ function updateDisplay(){
 }
 
 updateDisplay();
+
+buttons.addEventListener('click', (e) => {
+    const {target} = e;
+
+    if (!target.matches('button')){
+        return;
+    }
+
+    if (target.classList.contains('operator')){
+        console.log(`operator: ${target.value}`);
+        return;
+    }
+
+    if (target.classList.contains('decimal')){
+        console.log(`decimal: ${target.value}`);
+        return;
+    }
+
+    if (target.classList.contains('clear')){
+        console.log(`clear: ${target.value}`);
+        return;
+    }
+
+    // if (target.classList.contains('delete')){
+    //     console.log(`operator: ${target.value}`);
+    //     return;
+    // }
+
+    console.log(`number: ${target.value}`);
+});
+
+
+
+
+
 
