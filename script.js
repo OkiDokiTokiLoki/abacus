7d78e0952ffd141c4f6d228f3d3c198506bbf63d
tokiloki <codebadger1@gmail.com>
2021-12-16 18:10:33 +0200

    refactor button eventListeners

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelector('.buttons');
+let buttons = Array.from(document.querySelectorAll('.buttons'));
 const operationDisplay = document.querySelector('.operation');
 const resultDisplay = document.querySelector('.result');
 
@@ -15,38 +15,57 @@ function updateDisplay(){
 
 updateDisplay();
 
-buttons.addEventListener('click', (e) => {
-    const {target} = e;
 
-    if (!target.matches('button')){
-        return;
-    }
 
-    if (target.classList.contains('operator')){
-        console.log(`operator: ${target.value}`);
-        return;
-    }
-
-    if (target.classList.contains('decimal')){
-        console.log(`decimal: ${target.value}`);
-        return;
-    }
-
-    if (target.classList.contains('clear')){
-        console.log(`clear: ${target.value}`);
-        return;
-    }
-
-    // if (target.classList.contains('delete')){
-    //     console.log(`operator: ${target.value}`);
-    //     return;
-    // }
-
-    console.log(`number: ${target.value}`);
-});
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        console.log('clicked');
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.innerText);
+    })
+})
 
 
 
 
 
 
+
+
+// buttons.addEventListener('click', (e) => {
+//     const {target} = e;
+
+//     if (!target.matches('button')){
+//         return;
+//     }
+
+//     if (target.classList.contains('operator')){
+//         console.log(`operator: ${target.value}`);
+//         return;
+//     }
+
+//     if (target.classList.contains('decimal')){
+//         console.log(`decimal: ${target.value}`);
+//         return;
+//     }
+
+//     if (target.classList.contains('clear')){
+//         console.log(`clear: ${target.value}`);
+//         return;
+//     }
+
+//     // if (target.classList.contains('delete')){
+//     //     console.log(`operator: ${target.value}`);
+//     //     return;
+//     // }
+
+//     console.log(`number: ${target.value}`);
+// });
+
+
+
+
+
+
+// https://freshman.tech/calculator/
